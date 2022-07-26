53b5ee4b568612d5087c6fd1f7bbe73d20859753
Jigar Patel <its.jigar7@gmail.com>
2022-07-25 22:03:02 -0400

    final commit - all modules are up and working

--- src/components/user/UserSearch.jsx
+++ src/components/user/UserSearch.jsx
@@ -1,31 +1,28 @@
 import { useState, useContext } from 'react'
 import GithubContext from '../../context/github/GithubContex'
 import AlertContext from '../../context/alert/AlertContext'
+import { searchUsers } from '../../context/github/GithubAction'
 
 function UserSearch() {
   const [text, setText] = useState('')
 
-  const { users, searchUsers, clearUsers } = useContext(GithubContext)
+  const { users, dispatch } = useContext(GithubContext)
 
   const { setAlert } = useContext(AlertContext)
 
   const handleChange = (e) => setText(e.target.value)
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
 
     if (text === '') {
       setAlert('Please enter something', 'error')
     } else {
-      searchUsers(text)
+      dispatch({ type: 'SET_LOADING' })
+      const users = await searchUsers(text)
+      dispatch({ type: 'GET_USERS', payload: users })
       setText('')
     }
-  }
-
-  const handleClearBtn = (e) => {
-    // @todo - clear state
-    clearUsers()
-    setText('')
   }
 
   return (
@@ -54,7 +51,10 @@ function UserSearch() {
 
       {users.length > 0 && (
         <div>
-          <button className='btn btn-ghost btn-lg' onClick={handleClearBtn}>
+          <button
+            className='btn btn-ghost btn-lg'
+            onClick={() => dispatch({ type: 'CLEAR_USERS' })}
+          >
             Clear
           </button>
         </div>
